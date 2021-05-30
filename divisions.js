5347b34b749cffded7d6f93d547027ebcc4b0ec0
lakshya983 <82517136+lakshya983@users.noreply.github.com>
2021-05-30 13:02:41 +0530

    Add files via upload

---
+++ divisions.js
@@ -0,0 +1,10 @@
+class Divisions extends Ground {
+    display() {
+      var Color = color(random(0, 255), random(0, 255), random(0, 255));
+      var pos = this.body.position;
+      rectMode(CENTER);
+      fill(Color);
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  }
+  
